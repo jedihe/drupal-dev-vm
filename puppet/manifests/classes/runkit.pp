17fb709d7aa85b92e5fc65ddae4b6c231256388d
John Herreño <jedihe@gmail.com>
2014-07-23 11:12:06 -0500

    Refactor installation of runkit to use curl fetch and command.

--- puppet/manifests/classes/runkit.pp
+++ puppet/manifests/classes/runkit.pp
@@ -1,17 +1,19 @@
 class runkit {
-  exec { 'install runkit':
-    command => "${pear_proxy_prefix}sudo pecl install https://github.com/downloads/zenovich/runkit/runkit-1.0.3.tgz",
-    require => Package['php5'],
-    #creates => '/usr/lib/php5/20090626+lfs/xhprof.so',
-    unless => 'test -s /etc/php5/conf.d/runkit.ini',
-    #logoutput => true,
+  curl::fetch_and_command { "install runkit":
+    source      => "https://github.com/downloads/zenovich/runkit/runkit-1.0.3.tgz",
+    destination => "/tmp/runkit-1.0.3.tgz",
+    timeout     => 30,
+    command => "sudo pecl install runkit-1.0.3.tgz",
+    command_cwd => "/tmp",
+    command_unless => 'ls /etc/php5/conf.d/runkit.ini',
+    verbose     => false,
   }
 
   file { '/etc/php5/conf.d/runkit.ini':
     ensure => "present",
     content => "extension=runkit.so
 runkit.internal_override=1",
-    require => Exec['install runkit'],
+    require => Curl::Fetch_and_command['install runkit'],
     notify => Class['apache::service'],
   }
 }
