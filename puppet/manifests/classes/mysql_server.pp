3dc5dfea699d82f483c4d6304424022fbd7ad999
John Herreño <jedihe@gmail.com>
2014-08-30 22:32:43 -0500

    Add proper service management for MySQL within the container (using runit supervisor).
    Scripts were taken from https://github.com/fideloper/docker-mysql.git

--- puppet/manifests/classes/mysql_server.pp
+++ puppet/manifests/classes/mysql_server.pp
@@ -4,4 +4,34 @@ class mysql_server {
   class { 'mysql::server':
     config_hash => { 'root_password' => 'root' },
   }
+
+  file { '/etc/service/mysql':
+    ensure => "directory",
+    #mode => 755,
+    #owner => "vagrant",
+    #group => "vagrant",
+    #recurse => true,
+    require => Class['mysql::server'],
+  }
+
+  file { '/etc/service/mysql/run':
+    ensure => present,
+    source => "/vagrant/mysql.sh",
+    mode => 755,
+    require => File['/etc/service/mysql'],
+  }
+
+  file { '/var/lib/mysql':
+    ensure => "directory",
+    mode => 755,
+    recurse => true,
+    require => Class['mysql::server'],
+  }
+
+  file { '/etc/my_init.d/99_mysql_setup.sh':
+    ensure => present,
+    source => "/vagrant/99_mysql_setup.sh",
+    mode => 755,
+    require => Class['mysql::server'],
+  }
 }
