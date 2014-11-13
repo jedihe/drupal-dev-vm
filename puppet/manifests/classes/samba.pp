8e1d2c62c608fb661abff99193e4759f7c4850b8
John Herreño <jedihe@gmail.com>
2014-11-13 11:36:55 -0500

    Add vagrant user to Samba + add service resource for smbd and nmbd.

--- puppet/manifests/classes/samba.pp
+++ puppet/manifests/classes/samba.pp
@@ -12,5 +12,32 @@ class samba {
     #group => "vagrant",
     replace => true,
     require => Package['samba'],
+    notify => [Service['smbd', 'nmbd']],
+  }
+
+  exec { "vagrant user for samba":
+    command => "(echo vagrant; echo vagrant) | smbpasswd -a vagrant",
+    path => ['/bin', '/usr/bin'],
+  }
+
+  #exec { 'restart samba':
+  #  command => 'service smbd restart && service nmbd restart',
+  #  path => ['/usr/bin'],
+  #}
+
+  service { 'smbd':
+    name     => 'smbd',
+    ensure   => 'running',
+    enable   => true,
+    require  => File['/etc/samba/smb.conf'],
+    provider => 'upstart',
+  }
+
+  service { 'nmbd':
+    name     => 'nmbd',
+    ensure   => 'running',
+    enable   => true,
+    require  => File['/etc/samba/smb.conf'],
+    provider => 'upstart',
   }
 }
