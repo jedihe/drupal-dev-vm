85b9018fd1a3982cf281b7a0e87878e462665e58
John Herreño <jedihe@gmail.com>
2015-01-28 22:16:34 -0500

    Install PHP CodeSniffer and Drupal Coder module + add proper args for syntastic (Vim)

--- puppet/manifests/classes/devel.pp
+++ puppet/manifests/classes/devel.pp
@@ -4,6 +4,20 @@ class devel {
     ensure => installed,
   }
 
+  exec { 'phpcs':
+    command => 'pear install PHP_CodeSniffer-1.5.6',
+    unless => 'test -x /usr/bin/phpcs',
+    path => ['/bin', '/usr/bin'],
+    require => Package['php-pear'],
+  }
+
+  exec { 'drupal-coder-install':
+    command => 'drush pm-download coder --destination=/home/vagrant/.drush',
+    unless => 'test -d /home/vagrant/.drush/coder',
+    path => ['/bin', '/usr/bin'],
+    require => [Exec['install drush']],
+  }
+
   curl::fetch_and_command { "composer-install":
     source      => "https://getcomposer.org/installer",
     destination => "/tmp/composer-installer.php",
