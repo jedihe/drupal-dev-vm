c2c966a60f084dd7993176efcb30440a4f46ec62
John Herreño <jedihe@gmail.com>
2015-02-16 21:37:34 -0500

    Disable installation of runkit.

--- puppet/manifests/classes/runkit.pp
+++ puppet/manifests/classes/runkit.pp
@@ -1,19 +1,19 @@
 class runkit {
-  curl::fetch_and_command { "install runkit":
-    source      => "https://github.com/downloads/zenovich/runkit/runkit-1.0.3.tgz",
-    destination => "/tmp/runkit-1.0.3.tgz",
-    timeout     => 30,
-    command => "sudo pecl install runkit-1.0.3.tgz",
-    command_cwd => "/tmp",
-    command_unless => 'ls /etc/php5/conf.d/runkit.ini',
-    verbose     => false,
-  }
+  #curl::fetch_and_command { "install runkit":
+    #source      => "https://github.com/downloads/zenovich/runkit/runkit-1.0.3.tgz",
+    #destination => "/tmp/runkit-1.0.3.tgz",
+    #timeout     => 30,
+    #command => "sudo pecl install runkit-1.0.3.tgz",
+    #command_cwd => "/tmp",
+    #command_unless => 'ls /etc/php5/conf.d/runkit.ini',
+    #verbose     => false,
+  #}
 
-  file { '/etc/php5/conf.d/runkit.ini':
-    ensure => "present",
-    content => "extension=runkit.so
-runkit.internal_override=1",
-    require => Curl::Fetch_and_command['install runkit'],
-    notify => Class['apache::service'],
-  }
+  #file { '/etc/php5/conf.d/runkit.ini':
+    #ensure => "present",
+    #content => ";extension=runkit.so
+#;runkit.internal_override=1",
+    #require => Curl::Fetch_and_command['install runkit'],
+    #notify => Class['apache::service'],
+  #}
 }
