c91d1d75f809942e6912764a6af4224240fa613c
John Herreño <jedihe@gmail.com>
2015-11-02 22:53:07 -0500

    Install some tools: grunt, lua, terminus (Pantheon CLI)

--- puppet/manifests/classes/devel.pp
+++ puppet/manifests/classes/devel.pp
@@ -27,4 +27,42 @@ class devel {
     command_unless => 'test -x /usr/local/bin/composer',
     verbose     => false,
   }
+
+  package { 'lua5.2': ensure => installed }
+
+  # ==================================================
+  # Web Dev
+  # ==================================================
+  ppa_package { "node js":
+    ppa => 'ppa:chris-lea/node.js',
+    package => 'nodejs',
+  }
+
+  exec { 'grunt cli':
+    command => 'npm install -g grunt-cli',
+    unless => 'ls /usr/bin/grunt',
+    path => ['/bin', '/usr/bin'],
+    logoutput => true,
+    require => Ppa_package['node js'],
+  }
+
+  exec { 'bower cli':
+    command => 'npm install -g bower',
+    unless => 'ls /usr/bin/bower',
+    path => ['/bin', '/usr/bin'],
+    logoutput => true,
+    require => Ppa_package['node js'],
+  }
+
+  # Terminus (Pantheon CLI)
+  curl::fetch_and_command { "terminus-install":
+    source      => "https://github.com/pantheon-systems/cli/releases/download/0.9.2/terminus.phar",
+    destination => "/usr/local/bin/terminus",
+    timeout     => 60,
+    command => "chmod +x /usr/local/bin/terminus",
+    command_cwd => "/usr/local/bin",
+    command_unless => 'test -x /usr/local/bin/terminus',
+    verbose     => false,
+  }
+
 }
